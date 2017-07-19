846cd6226d7d8b9a4891a8a3192663197cf5c6f2
山崎裕貴 <todoyamatodozo@gmil.com>
2017-07-19 12:15:04 +0900

    棟の代入など　formの訂正

--- search.php
+++ search.php
@@ -1,15 +1,16 @@
 <?php
   require('dbconnect.php');
 
-  $sql = "SELECT * FROM `toilets`";
+  $sql_toilets = "SELECT * FROM `toilets`";
+  //チェックボックスの検索
   if(!empty($_POST['sort']) && isset($_POST['sort'])){
-    $sql = $sql . 'WHERE ';
-    $sql = $sql . $_POST['sort'][0] . '>0';
+    $sql_toilets = $sql_toilets. 'WHERE ';
+    $sql_toilets = $sql_toilets . $_POST['sort'][0] . '>0';
     for($i=1;$i<count($_POST['sort']);$i++){
-      $sql = $sql . ' AND ' . $_POST['sort'][$i] . '>0';
+      $sql_toilets = $sql_toilets . ' AND ' . $_POST['sort'][$i] . '>0';
     }
   }
-  $toilets = mysqli_query($db, $sql);
+  $toilets = mysqli_query($db, $sql_toilets);
   $array = array();
   while($rec = mysqli_fetch_assoc($toilets)){
     $array[] = $rec;

c3ffa7cc27b5fd840fa7f75709d0f89e3127d9cf
Masakazu Ankyu <mskz625@gmail.com>
2017-07-24 20:42:59 +0900

    不要ファイルの削除

--- dbconnect.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $db = mysqli_connect('localhost','root','','toilet')or die(mysqli_connect_error());
-  mysqli_set_charset($db,'utf8');
-?>
--- design/dbconnect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = mysqli_connect('localhost','root','','toiletweb')or die(mysqli_connect);
-mysqli_set_charset($db,'utf8');
- ?>

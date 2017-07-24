c260ffa5b5eae0c0846f92264f882376366d9ac5
Masakazu Ankyu <mskz625@gmail.com>
2017-07-24 20:43:15 +0900

    詳細ページの作成

---
+++ detail.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>ToiletBox | トイレ検索アプリケーション</title>
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/detail.css">
+  </head>
+  <body>
+    <a class="batu" href="index.php"><img src="assets/img/material/ic/cansel.png" alt="" ></a>
+    <div class="honbuto">
+      <ul>
+
+      </ul>
+      <div class="map_top">
+        <img class="school_map" src="assets/img/image/school_map.PNG" alt="map">
+      </div>
+
+    </div>
+    <div class="info">
+      <h2>INFORMATION</h2>
+      <img class="school_map" src="assets/img/image/info.PNG" alt="map">
+      <table>
+      <tr>
+        <th>和式</th> <td>iorgna</td>
+      </tr>
+      <tr>
+        <th>洋式</th> <td>flar</td>
+      </tr>
+      <tr>
+        <th>小べん</th> <td>nfoa</td>
+      </tr>
+      <tr>
+        <th>あたたかい</th> <td>klanre</td>
+      </tr>
+      <tr>
+        <th>おとひめ</th> <td>learn</td>
+      </tr>
+      <tr>
+        <th>ウォシュレット</th> <td>lkearn</td>
+      </tr>
+      </table>
+    </div>
+  </body>
+</html>

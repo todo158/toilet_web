0934fae2f6f71f1ff0a618dad1de02f2221418eb
山崎裕貴 <todoyamatodozo@gmil.com>
2017-10-04 00:03:33 +0900

    localhostからさくらサーバーにしました。確認お願いします。

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 //　参考サイト：https://qiita.com/fantm21/items/603cbabf2e78cb08133e
-$url = "http://localhost/toilet_web/toilets.json";
+$url = "http://tpu-wc-book.sakura.ne.jp/toilets.json";
 //ファイルの内容を読み込み
 $json = file_get_contents($url);
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
@@ -11,6 +11,5 @@ echo "<pre>";
 var_dump($arr["else"]["1"]["section"]);
 var_dump($arr["else"]["1"]["floor"]["1"]["1"]["male"]["style"]);
 echo "</pre>";
-
 
  ?>
